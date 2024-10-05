3dc085a415ad4ec652e721c2c11ee0eb8ecef070
Mario Carballo Zama <afk@ellugar.co>
2024-10-04 19:22:00 -0600

    fix: fix asm code

--- sys/sys-intrin.h
+++ sys/sys-intrin.h
@@ -4,6 +4,7 @@
 
 #ifdef TARGET_PLAYDATE
 #define bswap32 __builtin_bswap32
+
 static inline u32
 brev32(u32 v)
 {
@@ -14,16 +15,16 @@ brev32(u32 v)
 	return r;
 }
 
-// static inline i32
-// ssat16(i32 x)
-// {
-// 	u32 r = 0;
-// 	u32 i = (u32)x;
-// 	ASM("ssat %0, %1, %2"
-// 		: "=r"(r)
-// 		: "I"(16), "r"(i));
-// 	return (i32)r;
-// }
+static inline i32
+ssat16(i32 x)
+{
+	u32 r = 0;
+	u32 i = (u32)x;
+	__asm("ssat %0, %1, %2"
+		  : "=r"(r)
+		  : "I"(16), "r"(i));
+	return (i32)r;
+}
 #else
 static u32
 bswap32(u32 i)
@@ -44,7 +45,6 @@ brev32(u32 x)
 	return (r << s);
 }
 
-#endif
 static inline i32
 ssat16(i32 x)
 {
@@ -52,3 +52,5 @@ ssat16(i32 x)
 	if(x > I16_MAX) return I16_MAX;
 	return x;
 }
+
+#endif
