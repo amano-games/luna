930e7c36934391e18d6733acf034fb4121b63c09
afk <afk@ellugar.co>
2024-11-18 13:17:51 -0600

    feat: add ui_rectcut extensions

--- lib/layout.h
+++ lib/layout.h
@@ -2,14 +2,35 @@
 
 #include "mathfunc.h"
 
+enum ui_cut_side {
+	UI_CUT_NONE,
+	UI_CUT_LEFT,
+	UI_CUT_RIGHT,
+	UI_CUT_TOP,
+	UI_CUT_BOTTOM,
+};
+
 struct ui_rec {
 	i32 minx, miny, maxx, maxy;
+};
+
+struct ui_cut {
+	struct ui_rec *rect;
+	enum ui_cut_side side;
 };
 
 static inline rec_i32
 ui_rec_to_rec_i32(struct ui_rec v)
 {
 	return (rec_i32){v.minx, v.miny, v.maxx - v.minx, v.maxy - v.miny};
+}
+
+struct ui_cut
+ui_cut(struct ui_rec *rect, enum ui_cut_side side)
+{
+	return (struct ui_cut){
+		.rect = rect,
+		.side = side};
 }
 
 struct ui_rec
@@ -42,6 +63,49 @@ ui_cut_bottom(struct ui_rec *rect, i32 a)
 	i32 maxy   = rect->maxy;
 	rect->maxy = max_i32(rect->miny, rect->maxy - a);
 	return (struct ui_rec){rect->minx, rect->maxy, rect->maxx, maxy};
+}
+
+struct ui_rec
+ui_rect_cut(struct ui_cut ui_cut, i32 a)
+{
+	switch(ui_cut.side) {
+	case UI_CUT_LEFT: return ui_cut_left(ui_cut.rect, a);
+	case UI_CUT_RIGHT: return ui_cut_right(ui_cut.rect, a);
+	case UI_CUT_TOP: return ui_cut_top(ui_cut.rect, a);
+	case UI_CUT_BOTTOM: return ui_cut_bottom(ui_cut.rect, a);
+	default: {
+		BAD_PATH;
+		return (struct ui_rec){0};
+	}
+	}
+}
+
+struct ui_rec
+ui_get_left(const struct ui_rec *rect, i32 a)
+{
+	i32 maxx = min_i32(rect->maxx, rect->minx + a);
+	return (struct ui_rec){rect->minx, rect->miny, maxx, rect->maxy};
+}
+
+struct ui_rec
+ui_get_right(const struct ui_rec *rect, i32 a)
+{
+	i32 minx = max_i32(rect->minx, rect->maxx - a);
+	return (struct ui_rec){minx, rect->miny, rect->maxx, rect->maxy};
+}
+
+struct ui_rec
+ui_get_top(const struct ui_rec *rect, i32 a)
+{
+	i32 maxy = min_i32(rect->maxy, rect->miny + a);
+	return (struct ui_rec){rect->minx, rect->miny, rect->maxy, maxy};
+}
+
+struct ui_rec
+ui_get_bottom(const struct ui_rec *rect, i32 a)
+{
+	i32 miny = max_i32(rect->miny, rect->maxy - a);
+	return (struct ui_rec){rect->minx, miny, rect->maxy, rect->maxy};
 }
 
 void
