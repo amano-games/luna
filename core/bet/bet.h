69b412c3c3cb290695ec194ed6e28d3be4bf086a
afk <afk@ellugar.co>
2025-01-28 21:59:20 -0600

    fix: max bet nodes increased to 100

--- core/bet/bet.h
+++ core/bet/bet.h
@@ -3,7 +3,7 @@
 #include "mem.h"
 #include "sys-types.h"
 
-#define MAX_BET_NODES      40
+#define MAX_BET_NODES      100
 #define MAX_BET_CHILDREN   10
 #define MAX_BET_NODE_PROPS 4
 #define MAX_BET_NOTE       64
