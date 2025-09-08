60b61982b7899be10ecbe035f32183447a6d88f7
afk <afk@ellugar.co>
2025-09-08 17:44:07 -0600

    feat: add windows support

---
+++ win.mk
@@ -0,0 +1,80 @@
+space := $(null) $(null)
+ROOT_DIR := $(subst $(space),\ ,$(shell dirname "$(realpath $(firstword $(MAKEFILE_LIST)))"))
+
+include $(ROOT_DIR)/common.mk
+
+CC           := x86_64-w64-mingw32-cc
+DESTDIR      ?=
+PREFIX       ?=
+BINDIR       ?= ${PREFIX}win
+TARGET       := $(GAME_NAME).exe
+BUILD_DIR    := ${DESTDIR}${BINDIR}
+PLATFORM_DIR := platforms/win
+
+LDLIBS := -lm -lkernel32 -luser32 -lshell32 -ldxgi -ld3d11 -lole32 -lgdi32
+LDFLAGS :=
+
+WATCH_SRC   := $(shell find $(SRC_DIR) -name *.c -or -name *.s -or -name *.h)
+WATCH_SRC   += $(shell find $(LUNA_DIR) -name *.c -or -name *.s -or -name *.h)
+
+EXTERNAL_DIRS  := $(LUNA_DIR)/external $(LUNA_DIR)/external/sokol
+EXTERNAL_FLAGS := $(addprefix -isystem,$(EXTERNAL_DIRS))
+
+INC_DIRS       := $(shell find $(SRC_DIR) -type d)
+INC_DIRS       += $(LUNA_DIR) $(LUNA_DIR)/sys $(LUNA_DIR)/lib $(LUNA_DIR)/core
+INC_FLAGS      += $(addprefix -I,$(INC_DIRS))
+INC_FLAGS      += $(EXTERNAL_FLAGS)
+
+override CDEFS := $(CDEFS) -mwin32 -DBACKEND_SOKOL=1 -DSOKOL_D3D11 -DTARGET_WIN
+
+RELEASE_CFLAGS := ${CFLAGS}
+RELEASE_CFLAGS += -std=gnu11 -O2 -g3
+RELEASE_CFLAGS += -DNDEBUG
+RELEASE_CFLAGS += $(WARN_FLAGS)
+RELEASE_CFLAGS += -fno-omit-frame-pointer
+
+DEBUG_CFLAGS := -std=gnu11 -g3 -O0
+DEBUG_CFLAGS += $(WARN_FLAGS)
+DEBUG_CFLAGS += -DSOKOL_DEBUG=1
+DEBUG_CFLAGS += -DDEBUG=1
+# DEBUG_CFLAGS += -fsanitize-trap -fsanitize=address,unreachable
+
+DEBUG ?= 0
+ifeq ($(DEBUG), 1)
+CFLAGS := $(DEBUG_CFLAGS)
+else
+CFLAGS := $(RELEASE_CFLAGS)
+endif
+
+CFLAGS += $(CDEFS)
+
+ASSETS_OUT   := $(BUILD_DIR)/assets
+OBJS         := $(BUILD_DIR)/$(TARGET)
+PUBLISH_OBJS := $(BUILD_DIR)/$(GAME_NAME).zip
+
+.PHONY: all clean build steam run
+.DEFAULT_GOAL := all
+
+all: clean build run
+
+$(ASSETS_OUT): $(ASSETS_BIN)
+	mkdir -p $(ASSETS_OUT)
+	$(ASSETS_BIN) $(ASSETS_DIR) $(ASSETS_OUT)
+
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+	cp -fr $(PLATFORM_DIR)/. $(BUILD_DIR)
+
+$(OBJS): $(SRC_DIR)/main.c $(SHADER_OBJS) $(BUILD_DIR) $(ASSETS_OUT) $(WATCH_SRC)
+	$(CC) $(CFLAGS) $(INC_FLAGS) $< $(LDLIBS) $(LDFLAGS) -o $@
+
+clean:
+	rm -rf $(BUILD_DIR)
+
+run: build
+	cd $(BUILD_DIR) && wine ./$(TARGET)
+
+build: $(OBJS)
+release: clean $(PUBLISH_OBJS)
+publish: $(PUBLISH_OBJS)
+	butler push $(PUBLISH_OBJS) $(COMPANY_NAME)/$(GAME_NAME):win
