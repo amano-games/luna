eeb32da673379c4082ce2d741a02242c85583f33
afk <afk@ellugar.co>
2025-10-10 16:26:40 -0600

    fix: constant framerate

--- sys/sys-sokol.c
+++ sys/sys-sokol.c
@@ -1506,11 +1506,12 @@ sokol_write_recording(struct recording_1b *recording)
 	str8_list_pushf(scratch, &cmd_list, "-i -");
 
 	str8_list_pushf(scratch, &cmd_list, "-s %dx%d", w * scale, h * scale);
-	str8_list_pushf(scratch, &cmd_list, "-vf fps=%d", fps);
 	str8_list_pushf(scratch, &cmd_list, "-sws_flags neighbor");
 	str8_list_pushf(scratch, &cmd_list, "-c:v libx264");
 	str8_list_pushf(scratch, &cmd_list, "-pix_fmt yuv420p");
 	str8_list_pushf(scratch, &cmd_list, "-vb 2500k");
+	str8_list_pushf(scratch, &cmd_list, "-vsync cfr");
+	// str8_list_pushf(scratch, &cmd_list, "-vf fps=%d", fps);
 
 	str8_list_pushf(scratch, &cmd_list, "\"%s\"", path.str);
 
