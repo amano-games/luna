748c08ffca943c76d54dbee30c967cc6f3656eb6
afk <afk@ellugar.co>
2025-10-21 14:41:04 -0600

    feat: rec from aabb

--- base/rec.h
+++ base/rec.h
@@ -13,10 +13,22 @@ enum rec_cut_side {
 
 #define REC_UNPACK(r) r.x, r.y, r.w, r.h
 
-static inline struct rec_i32
-rec_i32_inset(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_from_aabb(i32 x0, i32 y0, i32 x1, i32 y1)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
+		.x = x0,
+		.y = y0,
+		.w = x1 - x0,
+		.h = y1 - y0,
+	};
+	return res;
+}
+
+static inline rec_i32
+rec_i32_inset(rec_i32 r, i32 a)
+{
+	rec_i32 res = {
 		.x = r.x + a,
 		.y = r.y + a,
 		.w = max_i32(0, r.w - 2 * a),
@@ -25,10 +37,10 @@ rec_i32_inset(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_inset_x(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_inset_x(rec_i32 r, i32 a)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
 		.x = r.x + a,
 		.y = r.y,
 		.w = max_i32(0, r.w - 2 * a),
@@ -37,10 +49,10 @@ rec_i32_inset_x(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_inset_y(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_inset_y(rec_i32 r, i32 a)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
 		.x = r.x,
 		.y = r.y + a,
 		.w = r.w,
@@ -49,10 +61,10 @@ rec_i32_inset_y(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_expand(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_expand(rec_i32 r, i32 a)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
 		.x = r.x - a,
 		.y = r.y - a,
 		.w = r.w + 2 * a,
@@ -61,10 +73,10 @@ rec_i32_expand(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_expand_x(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_expand_x(rec_i32 r, i32 a)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
 		.x = r.x - a,
 		.y = r.y,
 		.w = r.w + 2 * a,
@@ -73,10 +85,10 @@ rec_i32_expand_x(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_expand_y(struct rec_i32 r, i32 a)
+static inline rec_i32
+rec_i32_expand_y(rec_i32 r, i32 a)
 {
-	struct rec_i32 res = {
+	rec_i32 res = {
 		.x = r.x,
 		.y = r.y - a,
 		.w = r.w,
@@ -85,25 +97,25 @@ rec_i32_expand_y(struct rec_i32 r, i32 a)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_union(struct rec_i32 a, struct rec_i32 b)
+static inline rec_i32
+rec_i32_union(rec_i32 a, rec_i32 b)
 {
-	i32 x1             = min_i32(a.x, b.x);
-	i32 y1             = min_i32(a.y, b.y);
-	i32 x2             = max_i32(a.x + a.w, b.x + b.w);
-	i32 y2             = max_i32(a.y + a.h, b.y + b.h);
-	struct rec_i32 res = {x1, y1, x2 - x1, y2 - y1};
+	i32 x1      = min_i32(a.x, b.x);
+	i32 y1      = min_i32(a.y, b.y);
+	i32 x2      = max_i32(a.x + a.w, b.x + b.w);
+	i32 y2      = max_i32(a.y + a.h, b.y + b.h);
+	rec_i32 res = {x1, y1, x2 - x1, y2 - y1};
 	return res;
 }
 
-static inline struct rec_i32
-rec_intersection(struct rec_i32 a, struct rec_i32 b)
+static inline rec_i32
+rec_intersection(rec_i32 a, rec_i32 b)
 {
-	i32 x1             = max_i32(a.x, b.x);
-	i32 y1             = max_i32(a.y, b.y);
-	i32 x2             = min_i32(a.x + a.w, b.x + b.w);
-	i32 y2             = min_i32(a.y + a.h, b.y + b.h);
-	struct rec_i32 res = {
+	i32 x1      = max_i32(a.x, b.x);
+	i32 y1      = max_i32(a.y, b.y);
+	i32 x2      = min_i32(a.x + a.w, b.x + b.w);
+	i32 y2      = min_i32(a.y + a.h, b.y + b.h);
+	rec_i32 res = {
 		.x = x1,
 		.y = y1,
 		.w = max_i32(0, x2 - x1),
@@ -112,8 +124,8 @@ rec_intersection(struct rec_i32 a, struct rec_i32 b)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_clamp_inside(struct rec_i32 r, struct rec_i32 bounds)
+static inline rec_i32
+rec_i32_clamp_inside(rec_i32 r, rec_i32 bounds)
 {
 	if(r.x < bounds.x) {
 		r.w -= (bounds.x - r.x);
@@ -131,26 +143,26 @@ rec_i32_clamp_inside(struct rec_i32 r, struct rec_i32 bounds)
 }
 
 static inline i32
-rec_i32_right(struct rec_i32 r)
+rec_i32_right(rec_i32 r)
 {
 	return r.x + r.w;
 }
 
 static inline i32
-rec_i32_bottom(struct rec_i32 r)
+rec_i32_bottom(rec_i32 r)
 {
 	return r.y + r.h;
 }
 
 static inline v2_i32
-rec_i32_cntr(struct rec_i32 r)
+rec_i32_cntr(rec_i32 r)
 {
 	v2_i32 res = {.x = r.x + (r.w * 0.5f), .y = r.y + (r.h * 0.5f)};
 	return res;
 }
 
 static inline rec_i32
-rec_i32_pivot(struct rec_i32 r, v2 pivot)
+rec_i32_pivot(rec_i32 r, v2 pivot)
 {
 	rec_i32 res = {
 		.x = r.x - (r.w * pivot.x),
@@ -161,49 +173,49 @@ rec_i32_pivot(struct rec_i32 r, v2 pivot)
 	return res;
 }
 
-static inline struct rec_i32
-rec_i32_anchor(struct rec_i32 parent, struct rec_i32 r, struct v2 pivot)
+static inline rec_i32
+rec_i32_anchor(rec_i32 parent, rec_i32 r, struct v2 pivot)
 {
-	struct rec_i32 res = r;
-	res.x              = parent.x + (i32)((parent.w - r.w) * pivot.x);
-	res.y              = parent.y + (i32)((parent.h - r.h) * pivot.y);
+	rec_i32 res = r;
+	res.x       = parent.x + (i32)((parent.w - r.w) * pivot.x);
+	res.y       = parent.y + (i32)((parent.h - r.h) * pivot.y);
 	return res;
 }
 
-struct rec_i32
-rec_i32_cut_left(struct rec_i32 *r, i32 a)
+rec_i32
+rec_i32_cut_left(rec_i32 *r, i32 a)
 {
-	i32 cut_w          = min_i32(r->w, a);
-	struct rec_i32 res = {r->x, r->y, cut_w, r->h};
+	i32 cut_w   = min_i32(r->w, a);
+	rec_i32 res = {r->x, r->y, cut_w, r->h};
 	r->x += cut_w;
 	r->w -= cut_w;
 	return res;
 }
 
-struct rec_i32
-rec_i32_cut_right(struct rec_i32 *r, i32 a)
+rec_i32
+rec_i32_cut_right(rec_i32 *r, i32 a)
 {
-	i32 cut_w          = min_i32(r->w, a);
-	struct rec_i32 res = {r->x + r->w - cut_w, r->y, cut_w, r->h};
+	i32 cut_w   = min_i32(r->w, a);
+	rec_i32 res = {r->x + r->w - cut_w, r->y, cut_w, r->h};
 	r->w -= cut_w;
 	return res;
 }
 
-struct rec_i32
-rec_i32_cut_top(struct rec_i32 *r, i32 a)
+rec_i32
+rec_i32_cut_top(rec_i32 *r, i32 a)
 {
-	i32 cut_h          = min_i32(r->h, a);
-	struct rec_i32 res = {r->x, r->y, r->w, cut_h};
+	i32 cut_h   = min_i32(r->h, a);
+	rec_i32 res = {r->x, r->y, r->w, cut_h};
 	r->y += cut_h;
 	r->h -= cut_h;
 	return res;
 }
 
-struct rec_i32
-rec_i32_cut_bottom(struct rec_i32 *r, i32 a)
+rec_i32
+rec_i32_cut_bottom(rec_i32 *r, i32 a)
 {
-	i32 cut_h          = min_i32(r->h, a);
-	struct rec_i32 res = {r->x, r->y + r->h - cut_h, r->w, cut_h};
+	i32 cut_h   = min_i32(r->h, a);
+	rec_i32 res = {r->x, r->y + r->h - cut_h, r->w, cut_h};
 	r->h -= cut_h;
 	return res;
 }
@@ -222,5 +234,5 @@ rec_i32_cut(rec_i32 *r, enum rec_cut_side side, i32 a)
 	}
 
 error:
-	return (struct rec_i32){0};
+	return (rec_i32){0};
 }
