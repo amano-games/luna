4d0ec5d491ad0e4bccdb4c1c2f45cf6da5d7f160
afk <afk@ellugar.co>
2025-11-26 17:55:23 -0600

    fix: trace save to data file

--- base/trace.c
+++ base/trace.c
@@ -1,5 +1,7 @@
 #include "trace.h"
 #include "sys/sys-io.h"
+#include "base/log.h"
+#include "sys/sys.h"
 
 #if !defined(TRACE_AUTO)
 SPALL_NOINSTRUMENT
@@ -37,17 +39,14 @@ trace_init(str8 file_name, u8 *buffer, usize size)
 	int thread_id = 0;
 	spall_auto_thread_init(thread_id, SPALL_DEFAULT_BUFFER_SIZE);
 #else
-#if defined(TRACE)
-	log_info("Trace", "Init (Manual)");
-	void *f   = sys_file_open_w(file_name);
-	SPALL_CTX = spall_init_callbacks(1, &trace_fwrite, &trace_fflush, &trace_fclose, f, false);
-	// SPALL_CTX    = spall_init_file("pinball-trace.spall", 1);
+	log_info("trace", "Init (Manual)");
+	void *f      = sys_file_open_w(file_name);
+	SPALL_CTX    = spall_init_callbacks(1, &trace_fwrite, &trace_fflush, &trace_fclose, f, false);
 	SPALL_BUFFER = (SpallBuffer){
 		.length = size,
 		.data   = buffer,
 	};
 	spall_buffer_init(&SPALL_CTX, &SPALL_BUFFER);
-#endif
 #endif
 }
 
@@ -56,9 +55,7 @@ trace_buffer_close(void)
 {
 #if defined(TRACE_AUTO)
 #else
-#if defined(TRACE)
 	spall_buffer_quit(&SPALL_CTX, &SPALL_BUFFER);
-#endif
 #endif
 }
 
@@ -69,18 +66,12 @@ trace_close(void)
 	spall_auto_thread_quit();
 	spall_auto_quit();
 #else
-#if defined(TRACE)
 	spall_quit(&SPALL_CTX);
-#endif
 #endif
 }
 
 double
 trace_get_time_in_micros(void)
 {
-#if defined(TRACE)
 	return (((double)sys_seconds() * 1000000));
-#else
-	return 0;
-#endif
 }
