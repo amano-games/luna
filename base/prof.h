f3decc3b0d4b57d6df076fe8fd2564c4b822df9a
afk <afk@ellugar.co>
2026-02-19 20:33:28 -0600

    feat: make sure if there are multiple blocks with the same name they have a line number appended

--- base/prof.h
+++ base/prof.h
@@ -8,39 +8,45 @@
 
 #define PROF_ANCHOR_SIZE 4096
 
-struct prof_block {
-	str8 label;
-	// Total time this block was on the stack.
-	u32 us_total;
+struct prof_anchor {
+	u32 us_exclusive; // Does not include children
+	u32 us_inclusive; // Does include children
 
-	// Total time this block was at the top of the stack.
-	u32 us_exclusive;
-
-	// Number of frames for this block currently on the stack.
-	u32 frame_count;
-
-	// Total number of times we entered this block.
 	u32 hit_count;
+
+	const char *label;
 };
 
 struct prof_frame {
-	struct prof_block *block;
+	u16 anchor_idx;
+	u16 parent_idx;
+
 	u32 us_start;
+	u32 prev_us_inclusive;
+
 	u32 child_time;
+	const char *label;
 };
 
 struct prof {
+	u16 parent_idx;
+
 	u32 us_start;
 	u32 us_end;
-	struct prof_block blocks[PROF_ANCHOR_SIZE];
+
+	struct prof_anchor anchors[PROF_ANCHOR_SIZE];
 	struct prof_frame frames[64];
-	ssize block_count;
+	ssize anchor_count;
 	ssize frame_count;
 };
 
 static struct prof PROFILER;
 
-#define prof_block(name) prof_block_start(name, __COUNTER__ + 1)
+#define prof_stringize_2(x)    #x
+#define prof_stringize(x)      prof_stringize_2(x)
+#define prof_unique_name(name) name prof_stringize(__LINE__)
+
+#define prof_block(name) prof_block_start(prof_unique_name(name), __COUNTER__ + 1)
 /* #define prof_block(name) prof_block_start(name, \
  	({ static int i = -1; if (i == -1) i = prof_next_block_idx(); i; })) */
 
@@ -50,6 +56,8 @@ static void
 prof_ini(void)
 {
 	struct prof *prof = &PROFILER;
+	dbg_assert(ARRLEN(prof->anchors) < U16_MAX);
+	dbg_assert(ARRLEN(prof->frames) < U16_MAX);
 	mclr_struct(prof);
 	prof->us_start = sys_time_us();
 }
@@ -65,24 +73,27 @@ static ssize
 prof_next_block_idx(void)
 {
 	struct prof *prof = &PROFILER;
-	dbg_assert(prof->block_count < (ssize)ARRLEN(prof->blocks));
-	return prof->block_count++;
+	dbg_assert(prof->anchor_count < (ssize)ARRLEN(prof->anchors));
+	return prof->anchor_count++;
 }
 
 void
-prof_block_start(str8 label, ssize block_idx)
+prof_block_start(const char *label, ssize idx)
 {
 	struct prof *prof = &PROFILER;
-	dbg_assert(block_idx < (ssize)ARRLEN(prof->blocks));
-	struct prof_block *block = prof->blocks + block_idx;
-	block->label             = label;
-	block->hit_count++;
-	block->frame_count++;
+	dbg_assert(idx < (ssize)ARRLEN(prof->anchors));
+
+	struct prof_anchor *item = prof->anchors + idx;
 	dbg_assert(prof->frame_count < (ssize)ARRLEN(prof->frames));
 	prof->frames[prof->frame_count++] = (struct prof_frame){
-		.us_start = sys_time_us(),
-		.block    = block,
+		.prev_us_inclusive = item->us_inclusive,
+		.us_start          = sys_time_us(),
+		.parent_idx        = prof->parent_idx,
+		.anchor_idx        = idx,
+		.label             = label,
 	};
+
+	prof->parent_idx = idx;
 }
 
 void
@@ -91,23 +102,20 @@ prof_block_end(void)
 	u32 now_us        = sys_time_us();
 	struct prof *prof = &PROFILER;
 	if(prof->frame_count == 0) { return; } // if we just initialized in the middle a profiler then ignore this block
+
 	struct prof_frame *frame = &prof->frames[--prof->frame_count];
-	u32 time_total           = now_us - frame->us_start;
-	u32 time_exclusive       = time_total - frame->child_time;
 
-	// Add this frame's run time to the child time of its parent frame.
-	if(prof->frame_count) {
-		prof->frames[prof->frame_count - 1].child_time += time_total;
-	}
+	u32 elapsed      = now_us - frame->us_start;
+	prof->parent_idx = frame->parent_idx;
 
-	frame->block->us_exclusive += time_exclusive;
-	frame->block->frame_count--; // us_total counts only outermost invocations (collapses recursion)
+	struct prof_anchor *parent = prof->anchors + frame->parent_idx;
+	struct prof_anchor *anchor = prof->anchors + frame->anchor_idx;
 
-	// Update the block's total time, unless it still has another frame on the
-	// stack.
-	if(frame->block->frame_count == 0) {
-		frame->block->us_total += time_total;
-	}
+	parent->us_exclusive -= elapsed;
+	anchor->us_exclusive += elapsed;
+	anchor->us_inclusive = frame->prev_us_inclusive + elapsed;
+	++anchor->hit_count;
+	anchor->label = frame->label;
 }
 
 str8
@@ -130,24 +138,24 @@ prof_str8(struct alloc alloc)
 	f32 acc_perc = 0.0f;
 	dbg_assert(prof->frame_count == 0);
 
-	for(size_t i = 0; i < ARRLEN(prof->blocks); i++) {
-		struct prof_block *block = prof->blocks + i;
+	for(size_t i = 0; i < ARRLEN(prof->anchors); i++) {
+		struct prof_anchor *item = prof->anchors + i;
 
-		if(block->label.size == 0 || block->hit_count == 0) { continue; }
-		dbg_assert(block->frame_count == 0);
+		if(item->label == NULL || item->hit_count == 0) { continue; }
+		// dbg_assert(item->frame_count == 0);
 
-		f32 percent  = total_time ? (100.0f * (f32)block->us_exclusive / (f32)total_time) : 0.0f;
-		f32 avg_excl = (f32)block->us_exclusive / (f32)block->hit_count;
+		f32 percent  = total_time ? (100.0f * (f32)item->us_exclusive / (f32)total_time) : 0.0f;
+		f32 avg_excl = (f32)item->us_exclusive / (f32)item->hit_count;
 		acc_perc += percent;
 
 		str8_list_pushf(
 			alloc,
 			&list,
 			"%-12.12s %7" PRIu32 " %9" PRIu32 " %9" PRIu32 " %9.2f %5.1f%%",
-			block->label.str,
-			block->hit_count,
-			block->us_total,
-			block->us_exclusive,
+			item->label,
+			item->hit_count,
+			item->us_inclusive,
+			item->us_exclusive,
 			(double)avg_excl,
 			(double)percent);
 	}
