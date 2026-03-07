5731345f12451f2f50e79ce79ae107275bd4ddc2
afk <afk@ellugar.co>
2026-03-07 17:58:46 -0600

    fix: use linux_build_dev for steam build

--- build-steam.sh
+++ build-steam.sh
@@ -7,7 +7,7 @@ DOCKERFILE=./luna/Dockerfile
 podman build . -t $PROJECT_NAME -f $DOCKERFILE
 id=$(podman create $PROJECT_NAME)
 rm -rf ./build
-podman run -d --name "$id" -it $PROJECT_NAME:latest make linux_build DEBUG=0
+podman run -d --name "$id" -it $PROJECT_NAME:latest make linux_build_dev DEBUG=0
 podman cp "$id":/app/build ./build
 podman stop "$id"
 podman rm -v "$id"
