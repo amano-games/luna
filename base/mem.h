89be2db8134aaef249133625318ad7e66524d040
afk <afk@ellugar.co>
2026-03-30 17:37:59 -0600

    fix: remove mem trackin

--- base/mem.h
+++ base/mem.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "base/types.h"
-#include "base/utils.h"
 #include <stdalign.h>
 
 #define MKILOBYTE(value) ((value) * 1024LL)
@@ -16,14 +15,6 @@ struct alloc {
 	void *ctx;
 };
 
-struct alloc_meta {
-	const char *file;
-	int line;
-	const char *tag; // optional grouping label
-	ssize size;
-	void *ptr;
-};
-
 #define alloc_struct(alloc, ptr)     (__typeof__(ptr))alloc_size(alloc, sizeof(*(ptr)), alignof(__typeof__(*(ptr))), false)
 #define alloc_struct_clr(alloc, ptr) (__typeof__(ptr))alloc_size(alloc, sizeof(*(ptr)), alignof(__typeof__(*(ptr))), true)
 #define alloc_arr(alloc, ptr, count) \
@@ -31,34 +22,6 @@ struct alloc_meta {
 #define alloc_arr_clr(alloc, ptr, count) \
 	(__typeof__(ptr))alloc_size((alloc), sizeof(*(ptr)) * (count), alignof(__typeof__(*(ptr))), true)
 
-#if defined(DEBUG)
-#define DBG_ALLOCS_MAX 65536
-
-static struct alloc_meta DBG_MEM_ALLOCS[DBG_ALLOCS_MAX];
-static ssize DBG_MEM_ALLOCS_COUNT = 0;
-
-#define alloc_size(a, size, align, clr) \
-	alloc_size_dbg((a), (size), (align), (clr), __FILE__, __LINE__, NULL)
-
-static inline void *
-alloc_size_dbg(struct alloc alloc, ssize mem_size, ssize align, b32 clr, const char *file, int line, const char *tag)
-{
-	void *mem = alloc.allocf(alloc.ctx, mem_size, align);
-	if(clr) { mclr(mem, mem_size); }
-
-	if(mem && DBG_MEM_ALLOCS_COUNT < (ssize)ARRLEN(DBG_MEM_ALLOCS)) {
-		DBG_MEM_ALLOCS[DBG_MEM_ALLOCS_COUNT++] = (struct alloc_meta){
-			.file = file,
-			.line = line,
-			.tag  = tag,
-			.size = mem_size,
-			.ptr  = mem,
-		};
-	}
-
-	return mem;
-}
-#else
 static inline void *
 alloc_size(struct alloc alloc, ssize mem_size, ssize align, b32 clr)
 {
@@ -66,7 +29,6 @@ alloc_size(struct alloc alloc, ssize mem_size, ssize align, b32 clr)
 	if(clr) { mclr(mem, mem_size); };
 	return mem;
 }
-#endif
 
 // every object of type struct mkilobyte
 // will be aligned to 4-byte boundary
