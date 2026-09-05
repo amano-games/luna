05b0e015b05571519d5d7725eca5e842ab6c1ea1
afk <afk@ellugar.co>
2026-09-05 11:56:43 -0600

    change: get ready for removing the push_packed

--- tools/btree/btree.h
+++ tools/btree/btree.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "base/link-list.h"
 #include "lib/bet/bet.h"
 #include "base/types.h"
 
@@ -18,5 +19,49 @@ struct node_res {
 struct bet_node_holder {
 	struct bet_node *nodes;
 };
+
+struct bet_node_list_node {
+	struct bet_node_list_node *next;
+	struct bet_node bet_node;
+};
+
+struct bet_node_list {
+	struct bet_node_list_node *first;
+	struct bet_node_list_node *last;
+	u32 node_count;
+};
+
+static inline struct bet_node_list_node *
+bet_node_list_push_node(
+	struct bet_node_list *list,
+	struct bet_node_list_node *node)
+{
+	SLLQueuePush(list->first, list->last, node);
+	list->node_count += 1;
+	return (node);
+}
+
+static inline struct bet_node_list_node *
+bet_node_list_push_node_set_value(
+	struct bet_node_list *list,
+	struct bet_node_list_node *node,
+	struct bet_node bet_node)
+{
+	SLLQueuePush(list->first, list->last, node);
+	list->node_count += 1;
+	node->bet_node = bet_node;
+	return (node);
+}
+
+static inline struct bet_node_list_node *
+bet_node_list_push(
+	struct alloc alloc,
+	struct bet_node_list *list,
+	struct bet_node bet_node)
+{
+	struct bet_node_list_node *node = alloc_struct(alloc, node);
+	bet_node_list_push_node_set_value(list, node, bet_node);
+	return (node);
+}
 
 int handle_btree(str8 in_path, str8 out_path, struct alloc scratch);
